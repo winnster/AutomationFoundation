b810561c25070ba7f4b2e8ea6cc5c1de5e3bbce9
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2020-02-08 11:33:07 -0500

    Adding console logger package.

---
+++ src/AutomationFoundation.Runtime.Abstractions/Abstractions/IEngine.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+
+namespace AutomationFoundation.Runtime.Abstractions
+{
+    /// <summary>
+    /// Identifies an engine.
+    /// </summary>
+    public interface IEngine
+    {
+        /// <summary>
+        /// Starts the engine asynchronously.
+        /// </summary>
+        /// <returns>The asynchronous task.</returns>
+        Task StartAsync();
+
+        /// <summary>
+        /// Stops the engine asynchronously.
+        /// </summary>
+        /// <returns>The task to await.</returns>
+        Task StopAsync();
+
+        /// <summary>
+        /// Waits for the engine to stop.
+        /// </summary>
+        /// <returns>The task to await.</returns>
+        Task WaitForCompletionAsync();
+    }
+}
--- tools/ConsoleRunner/Program.cs
+++ tools/ConsoleRunner/Program.cs
@@ -2,54 +2,71 @@
 using Autofac.Extensions.DependencyInjection;
 using AutomationFoundation.Hosting;
 using AutomationFoundation.Hosting.Abstractions;
-using AutomationFoundation.Hosting.Abstractions.Builders;
 using ConsoleRunner.Abstractions;
 using ConsoleRunner.Infrastructure.IO;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace ConsoleRunner
 {
     public class Program
     {
-        private readonly IConsoleWriter output = new ConsoleWriter();
-        private readonly IConsoleReader input = new ConsoleReader();
+        private readonly IConsoleWriter output;
+        private readonly IConsoleReader input;
         private readonly IRuntimeHost host;
 
-        private Program(IRuntimeHost host)
+        private Program(IConsoleReader input, IConsoleWriter output, IRuntimeHost host)
         {
+            this.input = input ?? throw new ArgumentNullException(nameof(input));
+            this.output = output ?? throw new ArgumentNullException(nameof(output));
             this.host = host ?? throw new ArgumentNullException(nameof(host));
         }
 
-        private static IRuntimeHostBuilder CreateRuntimeHostBuilder()
+        private static IRuntimeHost CreateRuntimeHost()
         {
             return RuntimeHost.CreateBuilder<DefaultRuntimeHostBuilder>()
-                .ConfigureServices(services => services.AddAutofac())
-                .UseStartup<Startup>();
+                .ConfigureServices(services =>
+                {
+                    services.AddAutofac();
+                    services.AddLogging(logging => logging
+                        .AddConsole()
+                        .SetMinimumLevel(LogLevel.Information));
+                })
+                .UseStartup<Startup>()
+                .Build();
         }
 
         public static void Main()
         {
-            new Program(CreateRuntimeHostBuilder().Build())
-                .Run();
+            var reader = new ConsoleReader();
+            var writer = new ConsoleWriter();
+
+            try
+            {
+                var runtime = CreateRuntimeHost();
+
+                var p = new Program(reader, writer, runtime);
+                p.Run();
+            }
+            catch (Exception ex)
+            {
+                writer.WriteLine(ex.ToString(), ConsoleColor.Red);
+            }
+            finally
+            {
+                writer.WriteLine("Press any key to terminate...");
+                reader.WaitForAnyKey();
+            }
         }
 
         private void Run()
         {
-            try
-            {
-                host.Start();
+            host.Start();
 
-                output.WriteLine("Press any key to stop...");
-                input.WaitForAnyKey();
-
-                host.Stop();
-            }
-            catch (Exception ex)
-            {
-                output.WriteLine(ex.ToString(), ConsoleColor.Red);
-            }
-
-            output.WriteLine("Press any key to terminate...");
+            output.WriteLine("Press any key to stop...");
             input.WaitForAnyKey();
+
+            host.Stop();
         }
     }
 }
