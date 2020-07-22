37ab8ac738a81b8e4ab7715b9d7c6c20acedc782
Jeff Winn <6961614+jeff-winn@users.noreply.github.com>
2020-07-22 07:52:38 -0400

    Initial check-in.

---
+++ src/AutomationFoundation.Features.ProducerConsumer.Abstractions/IProducerEngine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using AutomationFoundation.Runtime.Abstractions;
+
+namespace AutomationFoundation.Features.ProducerConsumer.Abstractions
+{
+    /// <summary>
+    /// Identifies a producer engine.
+    /// </summary>
+    /// <typeparam name="TItem">The type of item being produced.</typeparam>
+    public interface IProducerEngine<TItem> : IStartable, IStoppable
+    {
+        /// <summary>
+        /// Initializes the engine.
+        /// </summary>
+        /// <param name="onProducedCallback">The callback to execute when an item is produced.</param>
+        /// <param name="parentToken">The parent cancellation token.</param>
+        void Initialize(Action<IProducerConsumerContext<TItem>> onProducedCallback, CancellationToken parentToken);
+    }
+}
